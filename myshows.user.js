f0e60fe2b52eed5fdb1efe7ed83b36097f2fb916
Vapauden Kuolemasta <kuolemasta@mail.ru>
2015-09-10 02:48:10 +0300

    Dirty func for drop dawn menu.

--- myshows.user.js
+++ myshows.user.js
@@ -1,4 +1,4 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @id 				myshows
 // @name 			Myshows Extentions
 // @version 		1.0
@@ -17,11 +17,29 @@
 		defaultValues : {
 			param : '720p',
 			threads : 5,
+			hrefList : [
+				{
+					id : 1,
+					status : 1,
+					name : 'ThePirateBay',
+					href : 'https://piratebay.to/search/%_SERIAL_NAME_%+%_SEASON_EPISODE_%+%_REQUEST_PARAM_%/0/3/0',
+					desc : 'Искать %_SERIAL_NAME_% %_SEASON_EPISODE_% %_REQUEST_PARAM_% на ThePirateBay',
+					icon : 'https://piratebay.to/static/img/tpblogo_sm_ny.gif',
+				},
+				{
+					id : 2,
+					status : 1,
+					name : 'Addic7ed',
+					href : 'http://www.addic7ed.com/search.php?search=%_SERIAL_NAME_%+%_SEASON_EPISODE_%',
+					desc : 'Искать субтитры к %_SERIAL_NAME_% %_SEASON_EPISODE_% на Addic7ed',
+					icon : 'http://cdn.addic7ed.com/favicon.ico',
+				},
+			],
 		},
 
 		getVar : function( param ){
 			if( this.ls.getItem( param ) === undefined || this.ls.getItem( param ) === null ){
-				this.ls.setItem( param , this.defaultValues[ param ] );
+				return this.defaultValues[ param ];
 			}
 			return this.ls.getItem( param );
 		},
@@ -42,19 +60,42 @@
 			for( param in this.defaultValues){
 				this.ls.setItem( param , this.defaultValues[ param ] );
 			}
+		},
+	}
+
+	function getEpisodesList(){
+		var list = document.querySelectorAll('.seasonBlockBody > table > tbody > tr');
+
+		var menu = settings.getVar('hrefList');
+		console.info(menu);
+
+		var tdInnerHtml = '<div class="buttonPopup _download _compact red"><ul>';
+		for( i=0; i<menu.length; i++ ){
+			var obj = menu[i];
+			tdInnerHtml += '<li><a target="_blank" href="'+(obj.href)+'"><img alt="img" class="fv_icon" src="'+obj.icon+'">'+(obj.desc)+'</a></li>';
+		}
+		tdInnerHtml += '</ul></div>';
+
+
+		for( i=0; i<list.length; i++ ){
+			var td = document.createElement('td');
+			td.innerHTML = tdInnerHtml
+			list[i].appendChild(td);
 		}
 	}
+	getEpisodesList();
+
 
 	// var ll = settings.getVarsList()
 	// console.info( ll.threads );
-	
+
 	queue = {
 		2677896 : 'Community+s06e09',
 	}
 
 	ajaxHandler = {
 
-		list : queue,
+		list : queue, // заменить на инит
 
 		getPage : function( episode ){
 			GM_xmlhttpRequest({
